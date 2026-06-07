8170b4b4b92331f32615f6ae233b2cb386c429f8
dr0431095@gmail.com <dr0431095@gmail.com>
2026-06-07 16:01:25 -0500

    Actualizar dominio canonico

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ingenieriafacilweb.com',
+  site: 'https://www.ingenieriafacilweb.com',
   integrations: [sitemap()],
 });
